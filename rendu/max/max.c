c1beefd967a29bdc45842e3068a3262d91760a30
Diahane619 <francevi@student.42firenze.it>
2023-03-06 14:24:10 +0100

    bd

---
+++ rendu/max/max.c
@@ -0,0 +1,17 @@
+int	max(int *tab, unsigned int len)
+{
+	unsigned int i;
+	int res;
+
+	res = tab[0];
+	i = 0;
+	if (len == 0)
+		return(0);
+	while (tab[i] && tab[i] <= len)
+	{
+		if(res < tab[i])
+			res = tab[i];
+		i++;
+	}
+	return (res);
+}

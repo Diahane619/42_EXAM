8a249d1c1e4b8be04cfe9afc44d499aa65c02ea9
Diahane619 <francevi@student.42firenze.it>
2023-03-31 19:05:10 +0200

    fgn

--- .system/grading/tester.sh
+++ .system/grading/tester.sh
@@ -6,12 +6,12 @@
 #    By: jcluzet <jcluzet@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/20 02:26:11 by jcluzet           #+#    #+#              #
-#    Updated: 2022/09/01 23:46:22 by jcluzet          ###   ########.fr        #
+#    Updated: 2022/09/01 23:46:12 by jcluzet          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-FILE='rstr_capitalizer.c'
-ASSIGN='rstr_capitalizer'
+FILE='print_hex.c'
+ASSIGN='print_hex'
 
 bash .system/auto_correc_program.sh $FILE $ASSIGN
 if [ -e .system/grading/traceback ];then
@@ -19,19 +19,19 @@ if [ -e .system/grading/traceback ];then
 	exit 1
 fi
 
-bash .system/auto_correc_program.sh $FILE $ASSIGN  "a FiRSt LiTTlE TESt"
+bash .system/auto_correc_program.sh $FILE $ASSIGN  "4324324"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_program.sh $FILE $ASSIGN "SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay, this is the last 1239809147801 but not    the least    t"
+bash .system/auto_correc_program.sh $FILE $ASSIGN  "98"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
 fi
 
-bash .system/auto_correc_program.sh $FILE $ASSIGN  "Okay i thing you got it!!! "
+bash .system/auto_correc_program.sh $FILE $ASSIGN  "42"
 if [ -e .system/grading/traceback ];then
     mv .system/grading/traceback .
 	exit 1
